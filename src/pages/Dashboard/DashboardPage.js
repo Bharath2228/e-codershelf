5be0c99e79e6c926873a22233a6ef22d3c7a2a3f
Bharath2228 <pbharath2210@gmail.com>
2024-08-01 16:32:22 +0530

    Update: DashboardPage Done.

--- src/pages/Dashboard/DashboardPage.js
+++ src/pages/Dashboard/DashboardPage.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react"
 import { DashbaordCard } from "./components/DashboardCard"
 import { DashbaordEmpty } from "./components/DashboardEmpty"
 
 export const DashbaordPage = () => {
 
-    const orders = [{}, {}]
+  const token = JSON.parse(sessionStorage.getItem("token"));
+  const ebid = JSON.parse(sessionStorage.getItem("ebid"));
+
+  const [ orders, setOrders] = useState([])
+
+    useEffect(() => {
+      async function fetchOrders(){
+        const response = await fetch(`http://localhost:8000/660/orders?user.id=${ebid}`, {
+          method: "GET",
+          headers: {"Content-Type": "application/json", Authorization: `Bearer ${token}`},
+          
+      })
+        const data = await response.json();
+        setOrders(data);
+      }
+      fetchOrders();
+    }, [])
 
     return (
       <main>
