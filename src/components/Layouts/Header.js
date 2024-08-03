2383924c6aa7414996cda83e43696c0aa55a1426
Bharath2228 <pbharath2210@gmail.com>
2024-08-03 21:07:42 +0530

    Dropdown bug fix

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../../assets/Bharath's Logo.png";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Search } from "../Sections/Search"
 import { DroppedDownLoggedOut, DroppedDownLoggedIn } from "../index";
 import { useCart } from "../../context";
@@ -11,7 +10,24 @@ export const Header = () => {
   const [ darkMode, setDarkMode ] = useState( JSON.parse(localStorage.getItem("darkMode")) || false);
   const [ searchSection, setSearchSection ] = useState(false)
   const [ dropDown, setDropDown ] = useState(false);  
+  const dropDownRef = useRef(null)
   const token = JSON.parse(sessionStorage.getItem("token"))
+
+  const handleClickOutside = (event) => {
+    if (dropDownRef.current && !dropDownRef.current.contains(event.target)) {
+      setDropDown(false);
+    }
+  }
+
+  useEffect(() => {
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
 
   useEffect(() => {
     localStorage.setItem("darkMode", JSON.stringify(darkMode))
@@ -23,11 +39,10 @@ export const Header = () => {
   },[darkMode])
 
   return (
-    <header>      
+    <header ref={dropDownRef}>      
       <nav className="bg-white dark:bg-gray-900">
           <div className="border-b border-slate-200 dark:border-b-0 flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-3">
               <Link to="/" className="flex items-center">
-                  <img src={Logo} className="mr-3 h-10" alt="E-CoderShelf Logo" />
                   <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">E-CoderShelf</span>
               </Link>
               <div className="flex items-center relative">
